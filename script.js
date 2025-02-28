3bd63ae69078f288a14243f4c6d9a89c840a87e4
SamuelFreitasSoares <samuelfreitassoares@outlook.com>
2025-02-28 17:30:01 -0300

    :tada: project finalized :tada:

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const container = document.getElementById("container");
 container.style.border = "1px solid"
 const defaultSize = 16;
 const bttn = document.getElementById("change-size");
-
+const reset = document.getElementById('reset');
 
 function generateBox(size,boxSize){
 
     for(let i = 1; i <= size*size; i++){
         let box = document.createElement("div");
         box.classList.add("colorbox");
-        box.style.border = "1px solid";
+        box.style.border = "1px solid lightgray";
         box.style.width = `${boxSize}px`;
         box.style.height = `${boxSize}px`;
         container.appendChild(box);
@@ -22,12 +22,15 @@ generateBox(defaultSize,603/defaultSize);
 
 bttn.addEventListener('click',()=>{
     let size = document.getElementById("size-value").value;
-    let boxSize = 603 / size
-    console.log("test");
+    if(size > 100){
+        alert("Size has to be between 16 and 100");
+    }else{
+    let boxSize = 603 / size;
     while(container.hasChildNodes()){
         container.removeChild(container.lastChild);
     }
     generateBox(size,boxSize);
+    }
 })
 
 let hoverBoxes = document.querySelectorAll(".colorbox");
@@ -40,7 +43,18 @@ document.addEventListener("mouseover", (event) => {
     }
 });
 
-
+reset.addEventListener('click',()=>{
+    let size = document.getElementById("size-value").value;
+    if(size > 100){
+        alert("Size has to be between 16 and 100");
+    }else{
+    let boxSize = 603 / size;
+    while(container.hasChildNodes()){
+        container.removeChild(container.lastChild);
+    }
+    generateBox(size,boxSize);
+    }
+})
 
 
 
